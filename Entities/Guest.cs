e2fa9ddb9a3155b3a9682288e2f30e92dcfd9484
Egor Gusarenko <volik46@gmail.com>
2019-09-20 10:11:27 +0300

    Basic data models added

---
+++ Entities/Guest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_GrahQL.Entities
+{
+    public class Guest
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(400)]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public string Patronimic { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public Guest()
+        {
+
+        }
+
+        public Guest(string name, DateTime registerDate)
+        {
+            Name = name;
+            RegisterDate = registerDate;
+        }
+
+        public Guest(string name, string surname, DateTime registerDate)
+        {
+            Name = name;
+            Surname = surname;
+            RegisterDate = registerDate;
+        }
+
+        public Guest(string name, string surname, string patronimic, DateTime registerDate)
+        {
+            Name = name;
+            Surname = surname;
+            Patronimic = patronimic;
+            RegisterDate = registerDate;
+        }
+    }
+}
