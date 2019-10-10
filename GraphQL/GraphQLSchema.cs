1a826ecb1313d66d57403f41c5162ea046da12a3
Egor Gusarenko <volik46@gmail.com>
2019-10-10 09:44:27 +0300

    Schema created

---
+++ GraphQL/GraphQLSchema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_GrahQL.GraphQL
+{
+    public class GraphQLSchema : Schema
+    {
+            public GraphQLSchema(IDependencyResolver resolver) : base(resolver)
+            {
+                Query = resolver.Resolve<MyHotelQuery>();
+            }
+    }
+}
