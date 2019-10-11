754d7e04fe4417614a4b27119795f85ad5cac362
Egor Gusarenko <volik46@gmail.com>
2019-10-11 10:57:14 +0300

    Update ReservationQuery.cs

--- GraphQL/ReservationQuery.cs
+++ GraphQL/ReservationQuery.cs
@@ -2,6 +2,7 @@
 using Angular_GrahQL.GraphQLTypes;
 using Angular_GrahQL.Repositories;
 using GraphQL.Types;
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 
@@ -9,10 +10,113 @@ namespace Angular_GrahQL.GraphQL
 {
     internal class ReservationQuery : ObjectGraphType
     {
-        public ReservationQuery(ReservationRepository reservationRepository)
+        public class ReservationQuery : ObjectGraphType
         {
-            
+            /*
+             -- Simple test query --
+                query TestQuery {
+                  reservations {
+                    id
+                    checkinDate
+                    checkoutDate
+                    guest {
+                      id
+                      name
+                      registerDate
+                    }
+                    room {
+                      id
+                      name
+                      number
+                      allowedSmoking
+                      status
+                    }
+                  }
+                }
+            */
 
+            public ReservationQuery(ReservationRepository reservationRepository)
+            {
+                /*Version: 1 get all*/
+                Field<ListGraphType<ReservationType>>("reservations",
+                    resolve: context => reservationRepository.GetQuery());
+
+                /*Version: 2 filtering*/
+                //Field<ListGraphType<ReservationType>>("reservations",
+                //    arguments: new QueryArguments(new List<QueryArgument>
+                //    {
+                //    new QueryArgument<IdGraphType>
+                //    {
+                //        Name = "id"
+                //    },
+                //    new QueryArgument<DateGraphType>
+                //    {
+                //        Name = "checkinDate"
+                //    },
+                //    new QueryArgument<DateGraphType>
+                //    {
+                //        Name = "checkoutDate"
+                //    },
+                //    new QueryArgument<BooleanGraphType>
+                //    {
+                //        Name = "roomAllowedSmoking"
+                //    },
+                //    new QueryArgument<RoomStatusType>
+                //    {
+                //        Name = "roomStatus"
+                //    }
+                //    }),
+                //    resolve: context =>
+                //    {
+                //        var query = reservationRepository.GetQuery();
+
+                //        var user = (ClaimsPrincipal)context.UserContext;
+                //        var isUserAuthenticated = ((ClaimsIdentity)user.Identity).IsAuthenticated;
+
+                //        var reservationId = context.GetArgument<int?>("id");
+                //        if (reservationId.HasValue)
+                //        {
+                //            if (reservationId.Value <= 0)
+                //            {
+                //                context.Errors.Add(new ExecutionError("reservationId must be greater than zero!"));
+                //                return new List<Reservation>();
+                //            }
+
+                //            return reservationRepository.GetQuery().Where(r => r.Id == reservationId.Value);
+                //        }
+
+                //        var checkinDate = context.GetArgument<DateTime?>("checkinDate");
+                //        if (checkinDate.HasValue)
+                //        {
+                //            return reservationRepository.GetQuery()
+                //                .Where(r => r.CheckinDate.Date == checkinDate.Value.Date);
+                //        }
+
+                //        var checkoutDate = context.GetArgument<DateTime?>("checkoutDate");
+                //        if (checkoutDate.HasValue)
+                //        {
+                //            return reservationRepository.GetQuery()
+                //                .Where(r => r.CheckoutDate.Date >= checkoutDate.Value.Date);
+                //        }
+
+                //        var allowedSmoking = context.GetArgument<bool?>("roomAllowedSmoking");
+                //        if (allowedSmoking.HasValue)
+                //        {
+                //            return reservationRepository.GetQuery()
+                //                .Where(r => r.Room.AllowedSmoking == allowedSmoking.Value);
+                //        }
+
+                //        var roomStatus = context.GetArgument<RoomStatus?>("roomStatus");
+                //        if (roomStatus.HasValue)
+                //        {
+                //            return reservationRepository.GetQuery().Where(r => r.Room.Status == roomStatus.Value);
+                //        }
+
+                //        return query.ToList();
+                //    }
+                );
+
+            }
         }
     }
 }
